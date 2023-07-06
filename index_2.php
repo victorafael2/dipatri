7ae7bd793333efb2ec00f6d3b812f521cf6124ca
Victor Rafael de Oliveira Silva <106404461+victorafael2@users.noreply.github.com>
2023-07-06 12:32:20 -0300

    google

--- index_2.php
+++ index_2.php
@@ -11,7 +11,7 @@ require_once 'vendor/autoload.php';
 // Configure as credenciais do Google
 $clientId = '937469059904-j4009pav3dl03q5cugi8f1sgm1h9m80q.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'https://dipatri.cloud/index_2.php';
+$redirectUri = 'https://dipatri.cloud/index.php';
 
 // Crie uma instância do cliente Google_Client
 $client = new Google_Client();
